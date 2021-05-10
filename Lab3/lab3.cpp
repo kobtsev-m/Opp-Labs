f337b6b641f6cd0d14af8726ba6d1d1634534e42
kobtsev-m <kobtsev.x@gmail.com>
2021-05-11 04:44:16 +0700

    Lab3 correction of matrix rows distribution between processes

--- Lab3/lab3.cpp
+++ Lab3/lab3.cpp
@@ -15,15 +15,6 @@ void fillData(double *A, double *B, int n1, int n2, int n3) {
     }
 }
 
-int calculateM(int n, int procTotal, int procRank) {
-    int m, idx = 0;
-    for (int i = 0; i < procRank + 1; ++i) {
-        m = (n - idx) / (procTotal - i);
-        idx += m;
-    }
-    return m;
-}
-
 void mulMatrix(double *A, double *B, double *C, int n1, int n2, int n3) {
     for (int i = 0; i < n1; ++i) {
         for (int j = 0; j < n3; ++j) {
@@ -34,23 +25,94 @@ void mulMatrix(double *A, double *B, double *C, int n1, int n2, int n3) {
     }
 }
 
-void printAnswer(double *C, int n1, int n3) {
-    for (int i = 0; i < n1; ++i) {
-        for (int j = 0; j < n3; ++j) {
-            printf("%.1f ", C[i*n3 + j]);
+int calculateChunkSize(int n, int procTotal, int procRank) {
+    int m = 0, idx = 0;
+    for (int i = 0; i < procRank + 1; ++i) {
+        idx += m;
+        m = (n - idx) / (procTotal - i);
+    }
+    return m;
+}
+
+void calculateDisplacements(
+    int *rowsSizes, int *rowsDisplacements,
+    int *colsSizes, int *colsDisplacements,
+    int *blocksSizes, int *blocksDisplacements,
+    int procRows, int procCols,
+    int n1, int n2, int n3
+) {
+    int rowsDisplacement = 0;
+    for (int x = 0; x < procRows; ++x) {
+        int xm = calculateChunkSize(n1, procRows, x);
+        rowsSizes[x] = xm * n2;
+        rowsDisplacements[x] = rowsDisplacement;
+        rowsDisplacement += xm * n2;
+    }
+    int colsDisplacement = 0;
+    for (int y = 0; y < procCols; ++y) {
+        int ym = calculateChunkSize(n3, procCols, y);
+        colsSizes[y] = ym * n2;
+        colsDisplacements[y] = colsDisplacement;
+        colsDisplacement += ym * n2;
+    }
+    int blocksDisplacement = 0;
+    for (int x = 0; x < procRows; ++x) {
+        for (int y = 0; y < procCols; ++y) {
+            int xm = calculateChunkSize(n1, procRows, x);
+            int ym = calculateChunkSize(n3, procCols, y);
+            blocksSizes[x*procCols + y] = xm * ym;
+            blocksDisplacements[x*procCols + y] = blocksDisplacement;
+            blocksDisplacement += xm * ym;
+        }
+    }
+}
+
+void correctResult(
+    double *tmpC, double *C,
+    int procRows, int procCols,
+    int n1, int n3
+) {
+    int k = 0, xIdx = 0, yIdx, xm, ym;
+    for (int x = 0; x < procRows; ++x) {
+        xm = calculateChunkSize(n1, procRows, x);
+        yIdx = 0;
+        for (int y = 0; y < procCols; ++y) {
+            ym = calculateChunkSize(n3, procCols, y);
+            for (int i = xIdx; i < xIdx + xm; ++i) {
+                for (int j = yIdx; j < yIdx + ym; ++j) {
+                    C[i*n3 + j] = tmpC[k++];
+                }
+            }
+            yIdx += ym;
+        }
+        xIdx += xm;
+    }
+}
+
+void printResult(double *C, int rowsN, int n, int m) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            printf("%.0f ", C[i*rowsN + j]);
         }
         printf("\n");
     }
     printf("\n");
 }
 
+void printWorkTime(double startTime, double endTime) {
+    printf("%.2f seconds\n", endTime - startTime);
+}
+
 int main(int argc, char *argv[]) {
 
     MPI_Init(&argc, &argv);
-
     MPI_Comm matrixComm, rowsComm, colsComm;
+
     int procRows = 4;
     int procCols = 4;
+    int n1 = 3000;
+    int n2 = 2000;
+    int n3 = 1000;
 
     int matrixDims[2] = {procRows, procCols};
     int matrixPeriods[2] = {false, false};
@@ -59,39 +121,47 @@ int main(int argc, char *argv[]) {
     int colsDims[2] = {0, procCols};
     int coords[2];
 
-    int n1 = 24;
-    int n2 = 16;
-    int n3 = 16;
-
     double *A = nullptr, *B = nullptr, *tmpC = nullptr, *C;
     double *blockA, *blockB, *blockC;
 
+    int xRank, yRank, rowsM, colsM;
+    int rowsSizes[procRows], rowsDisplacements[procRows];
+    int colsSizes[procCols], colsDisplacements[procCols];
+    int blocksSizes[procRows * procCols];
+    int blocksDisplacements[procRows * procCols];
+
     // Создание декартовой топологии размером procRows x procCols
     MPI_Cart_create(
-        MPI_COMM_WORLD,
-        2,
-        matrixDims,
-        matrixPeriods,
-        matrixReorder,
-        &matrixComm
+        MPI_COMM_WORLD, 2,
+        matrixDims, matrixPeriods,
+        matrixReorder, &matrixComm
     );
 
     // Выделение коммутаторов под строки и столбцы
     MPI_Cart_sub(matrixComm, rowsDims, &rowsComm);
     MPI_Cart_sub(matrixComm, colsDims, &colsComm);
 
-    // Получение координат текцщего процесса
+    // Получение координат текущего процесса
     MPI_Cart_get(matrixComm, 2, matrixDims, matrixPeriods, coords);
+    xRank = coords[0];
+    yRank = coords[1];
 
-    int xRank = coords[0];
-    int yRank = coords[1];
+    // Подсчёт количества строк / столбцов на процесс
+    rowsM = calculateChunkSize(n1, procRows, xRank);
+    colsM = calculateChunkSize(n3, procCols, yRank);
 
-    // Подсчёт количества строк и столбцов, выделяемых на процесс
-    int rowsM = calculateM(n1, procRows, xRank);
-    int colsM = calculateM(n2, procCols, yRank);
-    int blockSize = rowsM * colsM;
+    // Рассчёт сдвигов для процессов
+    if (xRank == 0 && yRank == 0) {
+        calculateDisplacements(
+            rowsSizes, rowsDisplacements,
+            colsSizes, colsDisplacements,
+            blocksSizes, blocksDisplacements,
+            procRows, procCols, n1, n2, n3
+        );
+    }
 
-    if (!xRank && !yRank) {
+    // Заполнение матриц
+    if (xRank == 0 && yRank == 0) {
         A = new double[n1 * n2];
         B = new double[n2 * n3];
         tmpC = new double[n1 * n3];
@@ -100,52 +170,60 @@ int main(int argc, char *argv[]) {
     }
 
     blockA = new double[rowsM * n2];
-    blockB = new double[colsM * n3];
-    blockC = new double[blockSize]();
+    blockB = new double[colsM * n2];
+    blockC = new double[rowsM * colsM]();
 
-    // Раздача строк матриц A и B по первому столдцу и первой строке процессов соответсенно
-    if (!yRank) {
-        MPI_Scatter(A, rowsM * n2, MPI_DOUBLE, blockA, rowsM * n2, MPI_DOUBLE, 0, rowsComm);
+    double startTime = MPI_Wtime();
+
+    // Раздача строк матриц A и B по первому столбцу и первой строке процессов
+    if (yRank == 0) {
+        MPI_Scatterv(
+            A, rowsSizes, rowsDisplacements, MPI_DOUBLE,
+            blockA, rowsM * n2, MPI_DOUBLE,
+            0, rowsComm
+        );
     }
-    if (!xRank) {
-        MPI_Scatter(B, colsM * n3, MPI_DOUBLE, blockB, colsM * n3, MPI_DOUBLE, 0, colsComm);
+    if (xRank == 0) {
+        MPI_Scatterv(
+            B, colsSizes, colsDisplacements, MPI_DOUBLE,
+            blockB, colsM * n2, MPI_DOUBLE,
+            0, colsComm
+        );
     }
 
-    // Ожидание завершения и раздача матриц останым процессам в декартовой системе
+    // Раздача матриц остальным процессам в декартовой системе
     MPI_Barrier(matrixComm);
     MPI_Bcast(blockA, rowsM * n2, MPI_DOUBLE, 0, colsComm);
-    MPI_Bcast(blockB, colsM * n3, MPI_DOUBLE, 0, rowsComm);
+    MPI_Bcast(blockB, colsM * n2, MPI_DOUBLE, 0, rowsComm);
 
-    // Ожидание завершения и подсчётов блока на каждом процессе
-    MPI_Barrier(matrixComm);
+    // Подсчёт блока на каждом процессе
     mulMatrix(blockA, blockB, blockC, rowsM, n2, colsM);
+    MPI_Barrier(matrixComm);
 
-    // Сборка матрицы C со всех процессов
-    MPI_Gather(blockC, blockSize, MPI_DOUBLE, tmpC, blockSize, MPI_DOUBLE, 0, matrixComm);
+    // Сбор матрицы C со всех процессов
+    MPI_Gatherv(
+        blockC, rowsM * colsM, MPI_DOUBLE,
+        tmpC, blocksSizes, blocksDisplacements, MPI_DOUBLE,
+        0, matrixComm
+    );
+
+    // Корректировка результатов после сбора матрицы MPI_Gatherv
+    if (xRank == 0 && yRank == 0) {
+        correctResult(tmpC, C, procRows, procCols, n1, n3);
+    }
+
+    double endTime = MPI_Wtime();
 
     delete[] blockA;
     delete[] blockB;
     delete[] blockC;
 
-    // Корректировка результатов после сбора матрицы MPI_Gather
-    if (!xRank && !yRank) {
-        int k = 0;
-        for (int x = 0; x < procRows; ++x) {
-            for (int y = 0; y < procCols; ++y) {
-                for (int i = x * rowsM; i < (x + 1) * rowsM; ++i) {
-                    for (int j = y * colsM; j < (y + 1) * colsM; ++j) {
-                        C[i * n3 + j] = tmpC[k++];
-                    }
-                }
-            }
-        }
-    }
-
-    // Вывод результата и очистка памяти
-    if (!xRank && !yRank) {
-        printAnswer(C, n1, n3);
+    if (xRank == 0 && yRank == 0) {
+        printResult(C, n3, 10, 10);
+        printWorkTime(startTime, endTime);
         delete[] A;
         delete[] B;
+        delete[] tmpC;
         delete[] C;
     }
 
